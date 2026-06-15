2c9ebfcc652bf76edb6b1e29c6126cc4a0377192
cholis <anoercholis@asia.ac.id>
2026-06-15 20:51:12 +0700

    modul 8 dan 9

--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HashMany;
 
 class Penduduk extends Model
 {
-    //
+    protected $fillable = ['nik', 'nama', 'jk', 'alamat'];
+    
+    public function surats(): HasMany
+    {
+    return $this->hasMany(Surat::class);
+    }
+
+    
 }
---
+++ resources/views/surat_index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('title', 'Data Surat')
+
+@section('content')
+<div class="card p-4">
+
+<div class="d-flex justify-content-between align-items-center mb-3">
+        <h3>Daftar Pengajuan Surat Kelurahan</h3>
+        
+        <!-- BUTTON TAMBAH: Mengarah ke rute surat.create -->
+        <a href="{{ route('surat.create') }}" class="btn btn-primary btn-sm">
+            <i class="fas fa-plus-circle-notch mr-1"></i> Tambah Pengajuan Surat
+        </a>
+    </div>
+
+    <!-- FLASH SESSION: Menampilkan notifikasi sukses setelah redirect -->
+    @if(session('sukses'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <i class="icon fas fa-check-circle mr-2"></i> {{ session('sukses') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+
+
+<table class="table table-striped table-bordered mt-3">
+<thead>
+<tr>
+<th>No Surat</th>
+<th>Jenis Surat</th>
+<th>Nama Pemohon</th>
+<th>NIK Pemohon</th>
+<th>Tanggal Ajuan</th>
+</tr>
+</thead>
+<tbody>
+@foreach($semuaSurat as $s)
+<tr>
+<td>{{ $s->nomor_surat }}</td>
+<td>{{ $s->jenis_surat }}</td>
+<td>{{ $s->penduduk->nama }}</td>
+<td>{{ $s->penduduk->nik }}</td>
+<td>{{ $s->tanggal_ajuan }}</td>
+</tr>
+@endforeach
+</tbody>
+</table>
+</div>
+@endsection
